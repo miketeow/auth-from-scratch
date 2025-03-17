0bce2b10e63d04b3f06bef20702b2fe45d199ff2
miketeow <27071053+miketeow@users.noreply.github.com>
2025-03-17 19:51:19 +0800

    add password hash and sign-up function

--- src/auth/actions.ts
+++ src/auth/actions.ts
@@ -2,8 +2,13 @@
 
 import { redirect } from "next/navigation";
 
+import { eq } from "drizzle-orm";
 import { z } from "zod";
 
+import { db } from "@/drizzle/db";
+import { UserTable } from "@/drizzle/schema";
+
+import { generateSalt, hashPassword } from "./core/password-hasher";
 import { signInSchema, signUpSchema } from "./schema";
 
 export async function signIn(unsafeData: z.infer<typeof signInSchema>) {
@@ -22,6 +27,29 @@ export async function signUp(unsafeData: z.infer<typeof signUpSchema>) {
     console.log(data);
     return "Unable to create account";
   }
+
+  const existingUser = await db.query.UserTable.findFirst({
+    where: eq(UserTable.email, data.email),
+  });
+
+  if (existingUser != null) return "Account already existed for this email";
+  const salt = generateSalt();
+  const hashedPassword = await hashPassword(data.password, salt);
+  try {
+    const [user] = await db
+      .insert(UserTable)
+      .values({
+        name: data.name,
+        email: data.email,
+        password: hashedPassword,
+        salt,
+      })
+      .returning({ id: UserTable.id, role: UserTable.role });
+    if (user == null) return "Unable to create account";
+  } catch {
+    return "Unable to create account";
+  }
+
   redirect("/");
 }
 export async function logOut() {
