17a42610c89f6855010c814e433f817f65302d6c
miketeow <27071053+miketeow@users.noreply.github.com>
2025-04-14 18:15:44 +0800

    add state and code verifier

--- src/app/api/oauth/[provider]/route.ts
+++ src/app/api/oauth/[provider]/route.ts
@@ -21,9 +21,10 @@ export async function GET(
 ) {
   const { provider: rawProvider } = await params;
   const code = request.nextUrl.searchParams.get("code");
+  const state = request.nextUrl.searchParams.get("state");
   const provider = z.enum(oAuthProviders).parse(rawProvider);
 
-  if (typeof code !== "string") {
+  if (typeof code !== "string" || typeof state !== "string") {
     redirect(
       `/sign-in?oauthError=${encodeURIComponent(
         "Failed to connect. Please try again"
@@ -31,7 +32,11 @@ export async function GET(
     );
   }
   try {
-    const oAuthUser = await new OAuthClient().fetchUser(code);
+    const oAuthUser = await new OAuthClient().fetchUser(
+      code,
+      state,
+      await cookies()
+    );
     const user = await connectUserToAccount(oAuthUser, provider);
     await createUserSession(user, await cookies());
   } catch (error) {
--- src/auth/core/oauth/base.ts
+++ src/auth/core/oauth/base.ts
@@ -1,8 +1,16 @@
+import { cookies } from "next/headers";
+
+import crypto from "crypto";
 import { z } from "zod";
 
 import { env } from "@/data/server";
 
 import { Cookies } from "../session";
+
+const STATE_COOKIE_KEY = "oAuthState";
+const CODE_VERIFIER_COOKIE_KEY = "oAuthCodeVerifier";
+// ten minutes in seconds
+const COOKIE_EXPIRATION_SECONDS = 60 * 10;
 
 export class OAuthClient<T> {
   // This is a standarised information from all providers
@@ -23,16 +31,31 @@ export class OAuthClient<T> {
   }
 
   createAuthUrl(cookies: Pick<Cookies, "set">) {
+    const state = createState(cookies);
+    const codeVerifier = createCodeVerifier(cookies);
     const url = new URL("https://discord.com/oauth2/authorize");
     url.searchParams.set("client_id", env.DISCORD_CLIENT_ID);
     url.searchParams.set("redirect_url", this.redirectUrl.toString());
     url.searchParams.set("response_type", "code");
     url.searchParams.set("scope", "identify email");
+    url.searchParams.set("state", state);
+    url.searchParams.set("code_challenge_method", "S256");
+    url.searchParams.set(
+      "code_challenge",
+      crypto.hash("sha256", codeVerifier, "base64url")
+    );
+
     return url.toString();
   }
 
-  async fetchUser(code: string) {
-    const { accessToken, tokenType } = await this.fetchToken(code);
+  async fetchUser(code: string, state: string, cookies: Pick<Cookies, "get">) {
+    const isValidState = await validateState(state, cookies);
+    if (!isValidState) throw new InvalidStateError();
+
+    const { accessToken, tokenType } = await this.fetchToken(
+      code,
+      getCodeVerifier(cookies)
+    );
     // URL is different for every providers
     const user = await fetch("https://discord.com/api/users/@me", {
       headers: {
@@ -55,7 +78,7 @@ export class OAuthClient<T> {
     };
   }
 
-  private fetchToken(code: string) {
+  private fetchToken(code: string, codeVerifier: string) {
     return fetch("https://discord.com/api/oauth2/token", {
       method: "POST",
       headers: {
@@ -68,6 +91,7 @@ export class OAuthClient<T> {
         grant_type: "authorization_code",
         client_id: env.DISCORD_CLIENT_ID,
         client_secret: env.DISCORD_CLIENT_SECRET,
+        code_verifier: codeVerifier,
       }),
     })
       .then((res) => res.json())
@@ -97,4 +121,48 @@ export class InvalidUserError extends Error {
     super("Invalid User");
     this.cause = zodError;
   }
+}
+
+export class InvalidStateError extends Error {
+  constructor() {
+    super("Invalid State");
+  }
+}
+
+export class InvalidCodeVerifierError extends Error {
+  constructor() {
+    super("Invalid Code Verifier");
+  }
+}
+function createState(cookies: Pick<Cookies, "set">) {
+  const state = crypto.randomBytes(64).toString("hex").normalize();
+  cookies.set(STATE_COOKIE_KEY, state, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "lax",
+    expires: Date.now() + COOKIE_EXPIRATION_SECONDS * 1000,
+  });
+  return state;
+}
+
+function createCodeVerifier(cookies: Pick<Cookies, "set">) {
+  const codeVerifier = crypto.randomBytes(64).toString("hex").normalize();
+  cookies.set(CODE_VERIFIER_COOKIE_KEY, codeVerifier, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "lax",
+    expires: Date.now() + COOKIE_EXPIRATION_SECONDS * 1000,
+  });
+  return codeVerifier;
+}
+
+function validateState(state: string, cookies: Pick<Cookies, "get">) {
+  const cookieState = cookies.get(STATE_COOKIE_KEY)?.value;
+  return cookieState === state;
+}
+
+function getCodeVerifier(cookies: Pick<Cookies, "get">) {
+  const codeVerifier = cookies.get(CODE_VERIFIER_COOKIE_KEY)?.value;
+  if (codeVerifier == null) throw new InvalidCodeVerifierError();
+  return codeVerifier;
 }
